df7aec0707ae4a2892a0bc8da8465548085daaa7
abbasalit987 <abbasalit987@gmail.com>
2023-03-29 15:51:38 +0530

    commit4/adding views for html

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
+const contactusRouter = require('./routes/contactus');
+const successRouter = require('./routes/success');
 
 const bodyParser = require('body-parser');
 
@@ -11,6 +13,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/admin',adminRouter);
 app.use('/shop',shopRouter);
+app.use(contactusRouter);
+app.use(successRouter);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Error 404 : Page Not Found</h1>');
--- routes/contactus.js
+++ routes/contactus.js
@@ -5,8 +5,8 @@ const express = require('express');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+router.get('/contactus', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'contactus.html'));
 });
 
 // /admin/add-product => POST
